4439473ca9f5d46cd80f0e20dfb28a1226df1dd3
Roger Ballard <rballard2@apple.com>
2025-09-08 14:20:44 -0700

    Vendor in swift-async-process.
    There's precedent for this in [swift-sdk-generator](https://github.com/swiftlang/swift-sdk-generator/tree/eae537f49e0620ac34ade033f8e2c2401b2e7998/Sources/AsyncProcess).

--- Package.swift
+++ Package.swift
@@ -25,14 +25,19 @@ let package = Package(
     products: [
         .library(
             name: "SwiftToolsSupportAsync",
-            targets: ["TSFFutures", "TSFUtility"]),
+            targets: ["TSFFutures", "TSFUtility", "TSFAsyncProcess"]),
         .library(
             name: "SwiftToolsSupportCAS",
             targets: ["TSFCAS", "TSFCASFileTree", "TSFCASUtilities"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.4"),
+        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.68.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.28.0"),
+        .package(url: "https://github.com/apple/swift-system.git", from: "1.1.1"),
         .package(url: "https://github.com/apple/swift-tools-support-core.git", "0.5.8"..<"0.8.0"),
     ],
     targets: [
@@ -43,6 +48,50 @@ let package = Package(
             cSettings: [
                 .headerSearchPath("./")
             ]
+        ),
+
+        // Async Process vendored library
+        .target(
+            name: "TSFAsyncProcess",
+            dependencies: [
+                "TSFProcessSpawnSync",
+                .product(name: "Atomics", package: "swift-atomics"),
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "DequeModule", package: "swift-collections"),
+                .product(name: "SystemPackage", package: "swift-system"),
+            ],
+            path: "Sources/AsyncProcess"
+        ),
+        .testTarget(
+            name: "TSFAsyncProcessTests",
+            dependencies: [
+                "TSFAsyncProcess",
+                .product(name: "Atomics", package: "swift-atomics"),
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "_NIOFileSystem", package: "swift-nio"),
+            ],
+            path: "Tests/AsyncProcessTests"
+        ),
+        .target(
+            name: "TSFCProcessSpawnSync",
+            path: "Sources/CProcessSpawnSync",
+            cSettings: [
+                .define("_GNU_SOURCE")
+            ]
+        ),
+        .target(
+            name: "TSFProcessSpawnSync",
+            dependencies: [
+                "TSFCProcessSpawnSync",
+                .product(name: "Atomics", package: "swift-atomics"),
+                .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+            ],
+            path: "Sources/ProcessSpawnSync"
         ),
 
         .target(
